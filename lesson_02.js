461917f7c94c1c5f1078c9c9859a308a5cf0b5eb
freddyasp21 <freddyperozo21@gmail.com>
2025-06-04 19:09:43 -0400

    Add JavaScript data types

---
+++ lesson_02.js
@@ -0,0 +1,36 @@
+// String (Cadena de texto)
+
+let nombre = "Juan";
+
+//Number
+
+let edad = 30;
+let high = 1.80;
+
+// Boolan
+
+let casado = true;
+let esMayorDeEdad = true;
+
+//Undefined
+
+let nombreCompleto;
+
+console.log(nombreCompleto)
+
+//Null
+
+let edadPadre = null;
+
+console.log(edadPadre)
+
+//Symbol
+
+let mySymbol = Symbol("Hola");
+
+console.log(mySymbol)
+
+//bitInt
+
+let bitInt = 10n;
+
